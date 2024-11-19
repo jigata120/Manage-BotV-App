c15fc3cd027cb76b5dfecde2d44c4572f536d183
Mihaeldzh <107353857+jigata120@users.noreply.github.com>
2024-11-19 23:52:30 +0200

    init

--- Users/urls.py
+++ Users/urls.py
@@ -1,11 +1,11 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
-
-
+from .views import RegisterView, LoginViewNew
 
 urlpatterns = [
-    path('profile/', views.profile, name='profile'),
-    path('sign-in/', views.login, name='login'),
-    path('sign-up/', views.register, name='register'),
-
+    path('profile/<int:pk>', views.ProfileView.as_view(), name='profile'),
+    path('sign-in/', LoginViewNew.as_view(), name='login'),
+    path('sign-up/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
