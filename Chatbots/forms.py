7fb87229039b8ceb66df67ce946447556286324b
Mihaeldzh <107353857+jigata120@users.noreply.github.com>
2024-11-29 23:56:17 +0200

    add: new forms and widgets

--- Chatbots/forms.py
+++ Chatbots/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm, RadioSelect, Textarea, TextInput
 
-from .models import Chatbot
+from .models import Chatbot, Settings, ContextData
 
 
 class CreateChatbotForm(ModelForm):
@@ -9,6 +9,8 @@ class CreateChatbotForm(ModelForm):
         fields = ('name','status','description')
         widgets = {
             'status': RadioSelect(choices=[(True, 'Active'), (False, 'Inactive')]),
+            'description': Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'name': TextInput(attrs={'class': 'form-control'}),
         }
 class EditChatbotForm(CreateChatbotForm):
     class Meta:
@@ -18,4 +20,23 @@ class EditChatbotForm(CreateChatbotForm):
             'status': RadioSelect(choices=[(True, 'Active'), (False, 'Inactive')]),
             'description': Textarea(attrs={'class': 'form-control', 'rows': 4}),
             'name': TextInput(attrs={'class': 'form-control'}),
+        }
+class EditChatbotSettingsForm(ModelForm):
+    class Meta:
+        model=Settings
+        fields = ('default_greeting','interaction_language','bot_personality',
+                  'context_length','output_length','limit_summary','rate_limit')
+
+
+class EditContextDataForm(ModelForm):
+    class Meta:
+        model= ContextData
+        fields = ('context_description','context_example')
+        widgets = {
+            'context_description': Textarea(attrs={'class': 'form-control', 'rows': 2}),
+            'context_example': Textarea(attrs={
+                'title': "Context",
+                'class': 'form-control',
+                'rows': 5
+            }),
         }
